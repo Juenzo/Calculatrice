9fec9d2d44cad13339b36476256ee70bbb38c0fb
Julien DE ALMEIDA <julien.de-almeida@student.junia.com>
2023-03-13 16:45:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function btm(val) {
+    document.getElementById("sortie").innerHTML += val;
+}
+function btmEfface(){
+   document.getElementById("sortie").innerHTML = "";
+}
+function btmAddition() {
+   document.getElementById("sortie").innerHTML += "+";
+}
+function btmSoustraction() {
+   document.getElementById("sortie").innerHTML += "-";
+}
+function btmMultiplication() {
+   document.getElementById("sortie").innerHTML += "*";
+}
+function btmDivision() {
+   document.getElementById("sortie").innerHTML += "/";
+}
+function btmparenthese() {
+    document.getElementById("sortie").innerHTML += "(";
+}
+
+function btmparenthese2() {
+    document.getElementById("sortie").innerHTML += ")";
+}
+
+function btmpoint() {
+    document.getElementById("sortie").innerHTML += ".";
+}
+
+function btmEgal() {
+     var egal = eval(document.getElementById('sortie').innerHTML);
+     document.getElementById('sortie').innerHTML = egal;
+}
+
